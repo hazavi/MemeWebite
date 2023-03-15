39712a903cbd53e21cf7a3a0223c1e064ca6c1ce
basse2620 <bassemunch@gmail.com>
2023-03-15 14:13:07 +0100

    Add project files.

---
+++ BlazorApp1/Data/Meme.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.SqlClient;
+
+namespace BlazorApp1.Data
+{
+    public class Meme
+    {
+
+        public int Id { get; set; }
+        public string MemeName { get; set; }
+        public string Url { get; set; }
+
+        
+    }
+}
---
+++ BlazorApp1/Data/Sql.cs
@@ -0,0 +1,44 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace BlazorApp1.Data
+{
+    public class Sql
+    {
+        string connectionstring = "Data Source=192.168.1.3;Initial Catalog=MemeDB;User ID=sa;Password=Passw0rd;";
+
+
+        public List<Meme> Read()
+        {
+            List<Meme> list = new List<Meme>();
+            SqlConnection con = new(connectionstring);
+            SqlCommand cmd = new SqlCommand("SELECT * FROM MemeTable", con);
+            con.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                Meme meme = new Meme()
+                { Id = (int)dr.GetInt32(0), MemeName = (string)dr.GetString(1), Url = dr.GetString(2)};
+                list.Add(meme);
+            }
+            con.Close();
+
+            return list;
+        }
+
+        public void Create(Meme meme)
+        {
+            using (SqlConnection conn = new(connectionstring))
+            {
+                var cmd = new SqlCommand($"INSERT INTO MemeTable (MemeName, Url) VALUE(@memeName, @memeUrl)", conn);
+                cmd.Parameters.Add("@memeName", SqlDbType.NVarChar).Value = meme.MemeName;
+                cmd.Parameters.Add("@memeUrl", SqlDbType.NVarChar).Value = meme.Url;
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+        }
+    }
+}
